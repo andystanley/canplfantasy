5f72dd76a7df8677c798a7db86fdb7a9eafd58fe
Andrew Stanley <andrew.stanley@shopify.com>
2019-05-21 19:59:28 -0400

    Added BuildSquad view

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import BuildSquad from './views/BuildSquad.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/build-squad',
+      name: 'build squad',
+      component: BuildSquad
     }
   ]
 })
---
+++ src/views/BuildSquad.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="build-squad">
+    <v-container>
+
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    components: { }
+  }
+</script>
