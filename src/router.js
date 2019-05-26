4f313d046901638b6360fd908d804ceac61eae5d
Andrew Stanley <andrew.stanley@shopify.com>
2019-05-26 13:18:54 -0400

    Changed BuildSquad to Squad and added navbar links

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import BuildSquad from './views/BuildSquad.vue'
+import Squad from './views/Squad.vue'
 
 Vue.use(Router)
 
@@ -15,9 +15,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/build-squad',
-      name: 'build squad',
-      component: BuildSquad
+      path: '/squad',
+      name: 'squad',
+      component: Squad
     }
   ]
 })
