eff17b329270bc40cf0e3eb8024c33c3c63acec8
KingCal <cdstanley20002@gmail.com>
2019-05-27 21:42:00 -0400

    added fromations with buttons

--- src/views/Squad.vue
+++ src/views/Squad.vue
@@ -41,6 +41,37 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <v-container fluid>
+      
+      <v-flex xs12 sm6 lg2>
+        
+            <h2>Chose a formation</h2>
+            <!--- so the value i dont really know what it is but i know that you need different names for each I also am think we could add images beside the the formation (3-5-2) -->
+            <v-btn-toggle dark>
+              <v-btn @click="threeFiveTwo" flat value="left">
+                <span> 3-5-3 </span>
+              </v-btn>
+              <v-btn @click="threeFourThree" flat value="center">
+                <span> 3-4-3 </span>
+              </v-btn>
+              <v-btn @click="fourFiveOne" flat value="right">
+                <span> 4-5-1 </span>
+              </v-btn>
+              <v-btn @click="fourFourTwo" flat value="justify">
+                <span> 4-4-2 </span>
+              </v-btn>
+              <v-btn @click="fourThreeThree" flat value="hello">
+                <span> 4-3-3 </span>
+              </v-btn>
+               <v-btn @click="fiveThreeTwo" flat value="why">
+                <span> 5-3-2 </span>
+              </v-btn>
+              <v-btn @click="fiveTwoThree" flat value="i dont know what this does">
+                <span> 5-2-3 </span>
+              </v-btn>
+            </v-btn-toggle>
+          </v-flex>
+        </v-container>
 
     <v-layout justify-center>
       <v-img src="/images/startingEleven.png" max-width="600">
