8383f8db738ce1ad5fb35de25d1ba2b67996d55d
Andrew Stanley <andrew.stanley@shopify.com>
2019-05-28 21:31:57 -0400

    Add player button

--- src/components/PlayerPopup.vue
+++ src/components/PlayerPopup.vue
@@ -16,18 +16,13 @@
         <v-card-text>
           <v-data-table :headers="headers" :items="players" class="elevation-1" :search="search" :pagination.sync="pagination">
             <template v-slot:items="props"> 
-              <tr >
-                <td style="cursor: pointer"  @click="addPlayer(props.item)">{{ props.item.first_name }} {{ props.item.last_name }}</td>
+              <tr>
+                <td class="no-padding"><v-btn icon small @click="addPlayer(props.item)"><v-icon>person_add</v-icon></v-btn></td>
+                <td>{{ props.item.first_name }} {{ props.item.last_name }}</td>
                 <td >{{ props.item.points }}</td>
                 <td >{{ props.item.price }}</td>
-                <td @click="props.expanded = !props.expanded"><v-img max-width="35" :src="props.item.team.shirt"></v-img></td>
-                <td @click="props.expanded = !props.expanded" style="cursor: pointer"><v-icon>expand_more</v-icon></td>
-                </tr>
-            </template>
-            <template v-slot:expand="props">
-               <v-card flat>
-               <v-card-text>Stats</v-card-text>
-               </v-card>
+                <td><v-img max-width="35" :src="props.item.team.shirt"></v-img></td>
+              </tr>
             </template>
           </v-data-table>  
         </v-card-text>
@@ -49,7 +44,8 @@ export default {
       playerName: '',
       search: '',
       headers: [
-        { text: 'Players', align: 'left', sortable: false, value: 'name'},
+        { sortable: false},
+        { text: 'Players', value: 'name'},
         { text: 'Points', value: 'points' },
         { text: 'Price', value: 'price' },
         { text: 'Team', value: 'team' }
@@ -74,3 +70,8 @@ export default {
 }
 </script>
 
+<style>
+  .no-padding {
+    padding: 0px !important;
+  }
+</style>
