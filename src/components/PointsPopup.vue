b6da0c61f0ec9048bf6d49acff3a30609ec63a12
Andrew Stanley <andrew.stanley@shopify.com>
2019-06-04 22:22:17 -0400

    Re-added PointsPopup

---
+++ src/components/PointsPopup.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="pointsPopup">
+    <v-layout justify-center>
+    {{ player.last_name }}
+    </v-layout>
+    
+   </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialPlayer: Object,
+  },
+   data () {
+    return {
+      player: !this.initialPlayer.hasOwnProperty('blank') ? this.initialPlayer : null,
+    }
+  },
+    
+}
+</script>
