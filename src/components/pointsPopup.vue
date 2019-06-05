5a7db7478f2c59689cb2470c7e8e13168802eda7
KingCal <cdstanley20002@gmail.com>
2019-06-04 22:17:17 -0400

    points page changes

--- src/components/pointsPopup.vue
+++ src/components/pointsPopup.vue
@@ -1,53 +1,23 @@
 <template>
-    <v-dialog max-width="600" v-model="popupOpen">
-      <template v-slot:activator="{ on }">
-          <v-icon :disabled="!player" color="red" class ="warning" small right @click="removePlayer">close</v-icon>
-          <v-layout justify-center> 
-            <v-img v-on="on" :src="player && player.team ? player.team.shirt : '/player/team/shirt'" style="cursor: pointer" max-width="80"></v-img>  
-          </v-layout>
-          <div class="darkGreen">
-            <v-layout justify-center>
-              <span :class="{'hide-text': !player}" class="font-weight-bold white--text " >{{ player ? player.last_name : 'position' }}</span>
-            </v-layout>
-            <hr>
-            <v-layout justify-center>
-              <span :class="{'hide-text': !player}" class="font-weight-bold white--text ">{{ player ? player.price : 'price' }}</span>
-            </v-layout>
-          </div>
-      </template>
-     
-    </v-dialog>
+   <div class="pointsPopup">
+    <v-layout justify-center>
+    {{ player.last_name }}
+    </v-layout>
+    
+   </div>
 </template>
 
 <script>
 export default {
   props: {
     initialPlayer: Object,
-    position: String,
-    players: Array,
-    addPlayerToSquad: Function,
-    removePlayerFromSquad: Function
+  },
+   data () {
+    return {
+      player: !this.initialPlayer.hasOwnProperty('blank') ? this.initialPlayer : null,
+    }
   },
     
-  methods : {
-    addPlayer(player) {
-      if (this.player) {
-        this.removePlayerFromSquad(this.player)
-      }
-      this.player = player
-      this.addPlayerToSquad(player)
-      this.popupOpen = false
-    },
-    }
 }
 </script>
 
-<style>
-  .no-padding {
-    padding: 0px !important;
-  }
-
-  .hide-text {
-    opacity: 0;
-  }
-</style>
--- src/views/Points.vue
+++ src/views/Points.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="points">
+    <div v-if="!squadExists">
+      <h1>Gameweek hasnt started</h1>
+    </div>
+    <div v-if="squadExists">
     <v-container fluid>
       <v-layout justify-space-around>
         <v-spacer></v-spacer>
 
          <v-flex>
-          <h2>Week {{ week }}1 rank</h2>
-          <h3> {{ rank }} 44 </h3>
+          <h2>Week 1 rank</h2>
+          <h3> 44 </h3>
         </v-flex>
         <v-flex>
           <h2>Total points</h2>
@@ -20,49 +24,51 @@
         <v-container fluid>
           <v-layout justify-space-around>
             <v-flex xs2 v-for="goalkeeper in squad.goalkeepers" :key="goalkeeper.id">
-             
+              
+              <PointsPopup :initialPlayer="goalkeeper"/>
             </v-flex>
           </v-layout>  
           
           <v-layout justify-space-around>
             <v-flex xs2 v-for="defender in squad.defenders" :key="defender.id">
-            
+              <PointsPopup :initialPlayer="defender"/>
             </v-flex>
           </v-layout>
 
           <v-layout justify-space-around>
             <v-flex xs2 v-for="midfielder in squad.midfielders" :key="midfielder.id">
-              
+              <PointsPopup :initialPlayer="midfielder"/>  
             </v-flex>
           </v-layout>
           
           <v-layout justify-space-around>
             <v-flex xs2 v-for="forward in squad.forwards" :key="forward.id">
-               
+                 <PointsPopup :initialPlayer="forward"/>  
             </v-flex>
           </v-layout>
         </v-container>   
       </v-img>
     </v-layout>
-
+  </div>
+  
     
     
     
   </div>
 </template>
-<script>
-export default {
-  components:{ },
-}
-</script>
-<script>
 
-
+<script>
+import { mapActions, mapState } from 'vuex'
+import PointsPopup from '@/components/PointsPopup'
 
 export default {
+  components:{
+    PointsPopup
+  },
  
   data() {
     return {
+      squadExists: false,
       squad: {
         goalkeepers: [{blank: true}],
         defenders: [{blank: true}, {blank: true}, {blank: true}, {blank: true}, {blank: true}],
@@ -72,6 +78,82 @@ export default {
      
     }
   },
+  methods:{
+    ...mapActions(['getProfile']),
+    
+    addPlayerToSquad(player) {
+      if (player.position.short_name =='GK') {
+        this.$set(this.squad.goalkeepers, 0, player)
+      }
+      else if (player.position.short_name =='DEF') {
+        const index = this.squad.defenders.findIndex(defender => defender.hasOwnProperty('blank'))        
+        this.$set(this.squad.defenders, index, player)
+      }
+      else if (player.position.short_name =='MID') {
+        const index = this.squad.midfielders.findIndex(midfielder => midfielder.hasOwnProperty('blank'))
+        this.$set(this.squad.midfielders, index, player)
+      }
+      else {
+        const index = this.squad.forwards.findIndex(forward => forward.hasOwnProperty('blank'))
+        this.$set(this.squad.forwards, index, player)
+      }
+      this.squadSize += 1
+      this.squadPrice -= Number(player.price)
+    },
+    setFormation(defenders, midfielders, forwards) {
+      while (defenders !== this.squad.defenders.length) {
+        if (defenders > this.squad.defenders.length) {
+          this.squad.defenders.push({blank: true})
+        } else {
+          const player = this.squad.defenders.pop()
+          if (!player.hasOwnProperty('blank')) {
+            this.removePlayerFromSquad(player)
+          }
+        }
+      }
+
+      while (midfielders !== this.squad.midfielders.length) {
+        if (midfielders > this.squad.midfielders.length) {
+          this.squad.midfielders.push({blank: true})
+        } else {
+          const player = this.squad.midfielders.pop()
+          if (!player.hasOwnProperty('blank')) {
+            this.removePlayerFromSquad(player)
+          }
+        }
+      }
+
+      while (forwards !== this.squad.forwards.length) {
+        if (forwards > this.squad.forwards.length) {
+          this.squad.forwards.push({blank: true})
+        } else {
+          const player = this.squad.forwards.pop()
+          if (!player.hasOwnProperty('blank')) {
+            this.removePlayerFromSquad(player)
+          }
+        }
+      }
+    },
+  },
+  computed: {
+    ...mapState(['profile']),
+    
+  },
+   created() {
+    this.getProfile()
+      .then(() => {
+        if (this.profile.active_squad) {
+          this.squadExists = true
+          const defenders = this.profile.active_squad.players.filter(player => player.position.short_name === 'DEF').length
+          const midfielders = this.profile.active_squad.players.filter(player => player.position.short_name === 'MID').length
+          const forwards = this.profile.active_squad.players.filter(player => player.position.short_name === 'FW').length
+          this.setFormation(defenders, midfielders, forwards)
+
+          this.profile.active_squad.players.map(player => this.addPlayerToSquad(player))
+        }
+      })
+  },
+
   
 }
 </script>
