57d9777067809af13517342021efc73a8d76428b
KingCal <cdstanley20002@gmail.com>
2019-06-05 20:26:20 -0400

    points page added with players

--- src/components/PointsPopup.vue
+++ src/components/PointsPopup.vue
@@ -1,8 +1,17 @@
 <template>
    <div class="pointsPopup">
     <v-layout justify-center>
-    {{ player.last_name }}
+    <v-img v-on="on" :src="player && player.team ? player.team.shirt : 'player.team.shirt'"  max-width="80"></v-img>  
     </v-layout>
+    <div class="darkGreen">
+            <v-layout justify-center>
+              <span :class="{'hide-text': !player}" class="font-weight-bold white--text " >{{ player ? player.last_name : 'position' }}</span>
+            </v-layout>
+            <hr>
+            <v-layout justify-center>
+              <span :class="{'hide-text': !player}" class="font-weight-bold white--text ">{{ player ? player.points : 'points' }}</span>
+            </v-layout>
+          </div>
     
    </div>
 </template>
