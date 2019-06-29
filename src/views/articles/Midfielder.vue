58ae38b704fa356f44239bd4de70c9b5dbd55bff
KingCal <cdstanley20002@gmail.com>
2019-06-29 14:17:14 -0400

    wording changed for midfield

--- src/views/articles/Midfielder.vue
+++ src/views/articles/Midfielder.vue
@@ -16,7 +16,7 @@
         </v-card-title>
 
         <v-card-text>
-         <p> Midfield the core of any good fantasy team these players will get you the most points, so it is very important that you chose them wisely. Saying that we have come up with some suggestions for you to help you pick these vital and key players. Like all of our lists, we will have a statistical ranking and some players to watch.</p>
+         <p> Midfield, the core of any good fantasy team. These players will get you the most points, so it is very important that you choose them wisely. Saying that we have come up with some suggestions to help you pick these key players. Like all of our lists, we will have a statistical ranking and some players to watch.</p>
           <div> <h3 style="width:75%"> Statistical Ranking</h3></div>
         
         <table style="width:100%">
@@ -77,16 +77,16 @@
         </table>
 
          
-          <p class="mt-4"> The players listed above and the players that have made the most fantasy points this season. This list would perhaps look very different if in some cases injuries had not stopped some players seasons short. Next, are some players to watch in the upcoming fall split.</p>
+          <p class="mt-4"> The players listed above are the players that have made the most fantasy points this season. This list would perhaps look very different if in some cases injuries had not stopped some players seasons short. Next, are some players to watch in the upcoming fall split.</p>
 
           <div> <h3 style="width:75%">Michael Petrasso</h3></div>
-          <p>Michael Petrasso the penalty taker and playmaker for Valour and also the linchpin in there team. Valour has not looked the same since Petrasso got hit with the injury bug and his playmaking and scoring abilities have been missed while he has been away or missed games they have been at a 0-4 record. Petrasso has been the star of the show when playing getting himself 1 goal and 2 assists in the four games he played in. </p>
+          <p>Michael Petrasso the penalty taker and playmaker for Valour and also the linchpin for the team. Valour has not looked the same since Petrasso got hit with the injury bug and his playmaking and scoring abilities have been missed while he has been away or missed games they have been at a 0-4 record. Petrasso has been the star of the show when playing getting himself 1 goal and 2 assists in the four games he played in. </p>
         
           <div> <h3 style="width:75%">Ryan Telfer</h3></div>
-          <p>Ryan Telfer a player with one of the best resumes in the canpl, but even with that resume of his he has underperformed. Ether blazing shots over the bar or playing in deeper midfielder roles Telfer has not been at his best of yet some may argue. But with Di Chiara coming back from injury we can potentially see Telfer moved back to where he played in the first game week against Forge where he did score. Telfer is also cheaper than anyone else on this page sitting at $6.5 thus the bar is lowered for more so the other players.</p>
+          <p> Ryan Telfer a player with one of the best resumes in the canpl, but even with that resume of his he has underperformed. Either blazing shots over the bar or playing in deeper midfielder roles Telfer has not been at his best of yet some may argue. But with Di Chiara coming back from injury we can potentially see Telfer moved back to where he played in the first game week against Forge where he did score. Telfer is also cheaper than anyone else on this page sitting at $6.5 thus the bar is lowered for more so the other players.</p>
           
           <div> <h3 style="width:75%">José Escalante</h3></div>
-          <p>José Escalante if anyone saw his performance against Valour they may have thought he would be one of the star players in the canpl this year but it seems that he never reached those heights again. Now with a new season and a new José Escalante, he should do much better and hopefully, he reaches those heights that we know he is capable of with a reasonable price of $7.0 you should be able to get a good band for your buck out of him.</p>
+          <p>Judging by José Escalante’s performance against Valour some may have thought he would be one of the star players in the canpl this year but it seems that he never reached those heights again. Now with a new season and a new José Escalante, he should do much better and hopefully, he reaches those heights that we know he is capable of. With a reasonable price of $7.0, he is one of the more affordable stars in the league. </p>
           
           
 
