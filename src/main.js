3c62641698229b623d6ecd08719c7bcba847048c
Andrew Stanley <andrew.stanley@shopify.com>
2019-08-03 21:23:10 -0400

    Fix analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+Vue.use(VueAnalytics, {
+  id: "UA-144995085-1",
+  router,
+  debug: {
+    enabled: !isProduction,
+    sendHitTask: isProduction
+  }
+});
 
 new Vue({
   router,
