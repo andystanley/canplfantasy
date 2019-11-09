faf09c77b6cbe2422354005ff3c941222041a874
Andrew Stanley <me@andrewstanley.ca>
2019-11-08 21:08:25 -0500

    Removed everything uneccessary to the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <v-app>
-    <NavBar />
     <v-content>
       <router-view></router-view>
     </v-content>
     <v-footer justify-space-around color="#fafafa" class="mb-3">
       <v-layout justify-center>
-        <a href="https://www.reddit.com/r/canplfantasy/" target="_blank">
-          <img
-            src="/images/redditImage.png"
-            alt="reddit"
-            height="35"
-            width="35"
-          />
-        </a>
-
         <a href="https://github.com/andystanley/canplfantasy" target="_blank">
           <img
             src="/images/github.png"
@@ -34,22 +24,7 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
-import NavBar from "@/components/NavBar";
-
 export default {
-  name: "App",
-  components: {
-    NavBar
-  },
-
-  created() {
-    this.getTeams();
-    this.getPlayers();
-  },
-
-  methods: {
-    ...mapActions(["getTeams", "getPlayers"])
-  }
+  name: "App"
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,5 @@
 <template>
   <v-container fluid>
     <h4 class="display-1 text-xs-center">CanPL Fantasy</h4>
-
-    <v-layout justify-center class="my-3">
-      <v-flex xs12 sm10 md8 lg6>
-        <v-card>
-          <v-img src="/images/forwardArticle.jpg"></v-img>
-          <v-card-title primary-title>
-            <h5 class="headline">Forwards to Watch: Fall Split</h5>
-          </v-card-title>
-
-          <v-card-actions class="pt-0">
-            <v-btn flat color="orange" to="/articles/forwards">Read More</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-
-    <v-layout justify-center class="my-3">
-      <v-flex xs12 sm10 md8 lg6>
-        <v-card>
-          <v-img src="/images/midfielderArticle.jpg"></v-img>
-          <v-card-title primary-title>
-            <h5 class="headline">Midfielders to Watch: Fall Split</h5>
-          </v-card-title>
-
-          <v-card-actions class="pt-0">
-            <v-btn flat color="orange" to="/articles/midfielders"
-              >Read More</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
   </v-container>
 </template>
