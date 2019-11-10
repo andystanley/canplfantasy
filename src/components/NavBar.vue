0f68275239480074c8c66547e7d7fc7b345b1c6c
Andrew Stanley <me@andrewstanley.ca>
2019-11-10 12:08:45 -0500

    Upgrade navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
-    <v-toolbar flat dense app>
-      <v-toolbar-side-icon
+    <v-app-bar flat dense>
+      <v-app-bar-nav-icon
         class="grey--text hidden-md-and-up"
         @click="drawer = !drawer"
-      ></v-toolbar-side-icon>
+      ></v-app-bar-nav-icon>
       <v-img
         src="/images/logo.png"
         max-width="45"
@@ -16,18 +16,18 @@
         <v-btn
           v-for="link in links"
           :key="link.text"
-          flat
-          router
           :to="link.route"
+          text
+          router
           >{{ link.text }}</v-btn
         >
       </v-toolbar-items>
       <v-spacer></v-spacer>
       <LoginSignupPopup v-if="!isAuthenticated" />
-      <v-btn v-if="isAuthenticated" flat class="primary" @click="logoutUser"
+      <v-btn v-if="isAuthenticated" text class="primary" @click="logoutUser"
         >Logout</v-btn
       >
-    </v-toolbar>
+    </v-app-bar>
 
     <v-navigation-drawer
       v-model="drawer"
@@ -37,19 +37,19 @@
       disable-resize-watcher
     >
       <v-list>
-        <v-list-tile
+        <v-list-item
           v-for="link in links"
           :key="link.text"
-          router
           :to="link.route"
+          link
         >
-          <v-list-tile-action>
+          <v-list-item-icon>
             <v-icon>{{ link.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ link.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
   </nav>
